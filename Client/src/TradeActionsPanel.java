365db850f8ee5e646b7df3ad819571292babefbf
Tenari the Great <djz24@case.edu>
2013-04-01 00:47:13 -0400

    Old Trade stuff thrown in. gross. 7/10

--- Client/src/TradeActionsPanel.java
+++ Client/src/TradeActionsPanel.java
@@ -1,25 +1,124 @@
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JList;
 import javax.swing.JPanel;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Tenari
- */
 class TradeActionsPanel extends JPanel implements ActionListener{
 
-    public TradeActionsPanel(ClientJApplet aThis) {
-    }
+    JComboBox yourStuff;
+	JList offeredStuff;
+	JButton accept;
+	JButton decline;
+	ClientJApplet client;
+	boolean updated;
+	ArrayList<String> list1;
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public TradeActionsPanel(ClientJApplet c) 
+    {
+    	super(new BorderLayout());
+    	client=c;
+    	list1=new ArrayList<>();
+    	list1.add("You Offer:");
+    	String[] list=new String[25];
+    	list[0]="You Offer:";
+    	offeredStuff=new JList(list);
+    	MouseListener mouseListener = new MouseAdapter() 
+    	{
+		     public void mouseClicked(MouseEvent e) 
+		     {
+		         int index = offeredStuff.locationToIndex(e.getPoint());
+		         if(index!=0)
+		         {
+		         	yourStuff.addItem(list1.get(index));
+		         	client.sendMessage("unoffer "+list1.get(index));
+		         	list1.remove(index);
+		         	String[] list2=new String[25];
+			    	for(int i=0;i<list1.size();i++)
+			    	{
+			    		list2[i]=list1.get(i);
+			    	}
+			    	
+			    	offeredStuff.setListData(list2);
+		         }
+		     }
+		 };
+ 		offeredStuff.addMouseListener(mouseListener);
+    	
+    	updated=false;
+    	
+    	accept=new JButton("Accept Trade");
+    	decline=new JButton("Decline Trade");
+    	
+    	accept.setActionCommand("accept");
+    	decline.setActionCommand("decline");
+    	decline.setEnabled(false);
+    	accept.addActionListener(this);
+    	decline.addActionListener(this);
+    	
+    	this.add(accept, BorderLayout.NORTH);
+    	this.add(decline, BorderLayout.SOUTH);
+    	
+    	this.add(offeredStuff, BorderLayout.WEST);
+    }
+    public void setMyStuff()
+    {
+    	if(updated)
+    	{
+    		
+    	}
+    	else
+    	{
+    		String[] arr=client.getInven().getMycrap();
+    		yourStuff=new JComboBox(arr);
+    		yourStuff.addActionListener(this);
+    		this.add(yourStuff, BorderLayout.EAST);
+    		updated=true;
+    	}
+    	
+    }
+    public void actionPerformed(ActionEvent e) 
+    {
+    	if ("accept".equals(e.getActionCommand())) 
+    	{
+    		client.sendMessage("acceptTrade");
+    		accept.setEnabled(false);
+    		decline.setEnabled(true);
+    	}
+    	else if("decline".equals(e.getActionCommand()))
+    	{
+    		client.sendMessage("unacceptTrade");
+    		accept.setEnabled(true);
+    		decline.setEnabled(false);
+    	}
+    	else
+    	{
+    		JComboBox cb = (JComboBox)e.getSource();
+	        String offerName = (String)cb.getSelectedItem();
+	        updateOffer(offerName);
+    	}
+        
+    }
+    
+    public void updateOffer(String name)
+    {
+    	list1.add(name);
+    	String[] list=new String[25];
+    	for(int i=0;i<list1.size();i++)
+    	{
+    		list[i]=list1.get(i);
+    	}
+    	
+    	offeredStuff.setListData(list);
+    	yourStuff.removeItem(name);
+    	client.sendMessage("offer "+name);
     }
     
 }
--- Client/src/TradeCanvas.java
+++ Client/src/TradeCanvas.java
@@ -1,18 +1,53 @@
-
-import java.awt.Canvas;
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
+ * @(#)Trade.java
  *
- * @author Tenari
+ *
+ * @author 
+ * @version 1.00 2010/2/16
  */
-class TradeCanvas extends Canvas{
 
-    public TradeCanvas() {
+import java.awt.*;
+import java.util.*;
+
+public class TradeCanvas extends Canvas {
+	ArrayList<String> opponentsOffer = new ArrayList<>();
+	String tradeIsGood;
+    public TradeCanvas() 
+    {
+    	tradeIsGood="a no-go.";
     }
     
+    public void addOppOffer(String s)
+    {
+    	opponentsOffer.clear();
+    	Scanner scan =new Scanner(s);
+    	tradeIsGood=scan.next();
+    	while(scan.hasNext())
+    	{
+    		opponentsOffer.add(scan.next());
+    	}
+    	
+    	if(tradeIsGood.equals("true"))
+    	{
+    		tradeIsGood="good to go!";
+    	}
+    	else
+    	{
+    		tradeIsGood="bad, you cheap-o!";
+    	}
+    }
+    
+    @Override
+    public void paint( Graphics window )
+	{
+		window.setColor(Color.BLACK);
+		window.drawString("Other Player's Offer",15,35);
+		window.drawString("Deal is "+tradeIsGood,15,19);
+		
+		for(int i=0;i<opponentsOffer.size();i++)
+		{
+			window.drawString(opponentsOffer.get(i), 20,(20*i)+50 );
+		}
+		
+	}
 }
